4532288db4577c508da7f656838df43599477136
kwinkich <vankord21@gmail.com>
2025-10-06 17:38:17 +0100

    feat: enhance Courses, Lessons, and Modules lists with data fetching and loading states

--- src/feature/education-feature/lessons-list/ui/List/index.tsx
+++ src/feature/education-feature/lessons-list/ui/List/index.tsx
@@ -1,6 +1,17 @@
-import { LessonCard } from "@/entities";
+import { LessonCard, lessonsQueryOptions } from "@/entities";
+import { useQuery } from "@tanstack/react-query";
 
 export const LessonsList = () => {
+	const {
+		data: lessonsData,
+		isLoading,
+		error,
+	} = useQuery(lessonsQueryOptions.list());
+
+	console.log("Lessons data:", lessonsData);
+	console.log("Loading:", isLoading);
+	console.log("Error:", error);
+
 	return (
 		<div className="flex flex-col gap-2">
 			{Array(10)
--- src/feature/education-feature/modules-list/ui/List/index.tsx
+++ src/feature/education-feature/modules-list/ui/List/index.tsx
@@ -1,6 +1,17 @@
-import { ModuleCard } from "@/entities";
+import { ModuleCard, modulesQueryOptions } from "@/entities";
+import { useQuery } from "@tanstack/react-query";
 
 export const ModulesList = () => {
+	const {
+		data: modulesData,
+		isLoading,
+		error,
+	} = useQuery(modulesQueryOptions.list());
+
+	console.log("Modules data:", modulesData);
+	console.log("Loading:", isLoading);
+	console.log("Error:", error);
+
 	return (
 		<div className="flex flex-col gap-2">
 			{Array(10)
