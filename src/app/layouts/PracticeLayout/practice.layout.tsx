f9c8ac55dfd220a33f12fd4919579ec109fe326b
Alisher Sadykov <alishersadykov12354@gmail.com>
2025-10-09 20:14:24 +0900

    small fixes

--- src/app/layouts/PracticeLayout/practice.layout.tsx
+++ src/app/layouts/PracticeLayout/practice.layout.tsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { RoleNavBar } from "@/widget";
 
 export const PracticeLayout = () => {
+  const location = useLocation();
+  const isCreatePage = location.pathname.startsWith("/practice/create");
   return (
 		<div className="bg-white relative">
-			<div className="w-full h-full pb-16 bg-second-bg">
+			<div className={`w-full h-full ${isCreatePage ? "pb-0" : "pb-16"} bg-second-bg`}>
 				<Outlet />
 			</div>
-			<RoleNavBar />
+			{<RoleNavBar />}
 		</div>
   );
 };
