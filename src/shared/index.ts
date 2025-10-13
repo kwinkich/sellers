290a5e81f0cb54519cca11464724519ed6626f31
Kislitsin Aleksei <aakislitsin@gmail.com>
2025-10-13 20:42:09 +0300

    small fixes

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,6 +1,5 @@
 export * from "./api/index";
 export * from "./hooks/useInit";
-export * from "./hooks/useZoomConnection";
 export * from "./icons/index";
 export * from "./lib/index";
 export * from "./types/index";
